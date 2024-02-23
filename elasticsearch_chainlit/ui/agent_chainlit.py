31c2c48a5dd76e1b44e7a144b616ed318fb3c769
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2024-02-23 19:09:49 +0800

    Changed ElasticSearch to Elasticsearch

--- elasticsearch_chainlit/ui/agent_chainlit.py
+++ elasticsearch_chainlit/ui/agent_chainlit.py
@@ -15,7 +15,7 @@ async def on_chat_start():
     agent = agent_factory()
     cl.user_session.set(KEY_AGENT, agent)
     cl.user_session.set(KEY_USER_QUESTIONS, [])
-    await cl.Message(content="ElasticSearch Agent started. Please check the README section to see how the agent can be used.").send()
+    await cl.Message(content="Elasticsearch Agent started. Please check the README section to see how the agent can be used.").send()
 
 
 @cl.on_message
